74a7b04dd894417366edb070825090aed06b72ea
nhuttommy <nhuttommy@gmail.com>
2022-07-11 18:24:38 +0700

    contract development

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -1,15 +1,7 @@
 pragma solidity ^0.4.25;
-
-// It's important to avoid vulnerabilities due to numeric overflow bugs
-// OpenZeppelin's SafeMath library, when used correctly, protects agains such bugs
-// More info: https://www.nccgroup.trust/us/about-us/newsroom-and-events/blog/2018/november/smart-contract-insecurity-bad-arithmetic/
-
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
-/************************************************** */
-/* FlightSurety Smart Contract                      */
-/************************************************** */
 contract FlightSuretyApp
 {
     // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
